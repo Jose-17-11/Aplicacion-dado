dd017e037b116ebc8165409ec9d9926dd622bb4b
Jose-17-11 <ariashdez@outlook.com>
2024-06-28 13:03:28 -0500

    first commit dado

---
+++ app/src/main/java/com/example/practica_01/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.practica_01
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView =findViewById(R.id.dice_image)
+
+        rollButton.setOnClickListener{
+            val randomInt = (1..6).random()
+            val drawableResource = when (randomInt){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+
+
+        }
+            val dice_image:ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+        }
+    }
+}
